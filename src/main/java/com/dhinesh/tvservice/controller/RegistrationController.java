7cf1d2c801fb2509e88e37ee7053ced2b4ebaae4
dhineshL <dhineshdl22@gmail.com>
2022-04-09 17:10:44 +0530

    Removing unwanted imports

--- src/main/java/com/dhinesh/tvservice/controller/RegistrationController.java
+++ src/main/java/com/dhinesh/tvservice/controller/RegistrationController.java
@@ -2,12 +2,14 @@ package com.dhinesh.tvservice.controller;
 
 import com.dhinesh.tvservice.model.UserModel;
 import com.dhinesh.tvservice.service.ApplicationService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -22,8 +24,8 @@ public class RegistrationController {
         this.applicationService = applicationService;
     }
 
-    @PostMapping(value = "register",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createUser(@Valid @RequestBody UserModel userModel){
+    @PostMapping(value = "register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserModel userModel) {
 
         applicationService.createUser(userModel);
 
--- src/main/java/com/dhinesh/tvservice/controller/UserController.java
+++ src/main/java/com/dhinesh/tvservice/controller/UserController.java
@@ -1,20 +1,14 @@
 package com.dhinesh.tvservice.controller;
 
-import com.dhinesh.tvservice.consumer.TvMazeConsumer;
-import com.dhinesh.tvservice.model.TvShow;
 import com.dhinesh.tvservice.model.TvShowModel;
-import com.dhinesh.tvservice.model.UserModel;
 import com.dhinesh.tvservice.service.ApplicationService;
-import com.dhinesh.tvservice.service.TvService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 
@@ -31,35 +25,35 @@ public class UserController {
     }
 
 
-    @GetMapping(value = "suggest",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "suggest", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAnyRole('ROLE_USER')")
-    public ResponseEntity<TvShowModel> suggestShow(Principal principal){
+    public ResponseEntity<TvShowModel> suggestShow(Principal principal) {
 
         TvShowModel tvShow = applicationService.suggestTvShow(principal);
 
-        return new ResponseEntity<>(tvShow,HttpStatus.OK);
+        return new ResponseEntity<>(tvShow, HttpStatus.OK);
     }
 
-    @GetMapping(value="streamingnow",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "streamingnow", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAnyRole('ROLE_USER')")
-    public ResponseEntity<List<TvShowModel>> getTvShowsToday(){
+    public ResponseEntity<List<TvShowModel>> getTvShowsToday() {
         List<TvShowModel> tvShows = applicationService.getTvShowsToday();
 
-        return new ResponseEntity<>(tvShows,HttpStatus.OK);
+        return new ResponseEntity<>(tvShows, HttpStatus.OK);
     }
 
-    @PostMapping(value="like",produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "like", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAnyRole('ROLE_USER')")
-    public ResponseEntity<?> likedTvShow(@RequestBody TvShowModel tvShowModel , Principal principal){
-        applicationService.likeTvShow(tvShowModel,principal);
+    public ResponseEntity<?> likedTvShow(@RequestBody TvShowModel tvShowModel, Principal principal) {
+        applicationService.likeTvShow(tvShowModel, principal);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping(value="top",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "top", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAnyRole('ROLE_USER')")
-    public ResponseEntity<List<TvShowModel>> getTop10TvShows(){
+    public ResponseEntity<List<TvShowModel>> getTop10TvShows() {
         List<TvShowModel> tvShows = applicationService.getTop10TvShow();
 
-        return new ResponseEntity<>(tvShows,HttpStatus.OK);
+        return new ResponseEntity<>(tvShows, HttpStatus.OK);
     }
 }
